7d7a3cd78a5882e8d93c800d9e46cc9b2ed8b884
JunnYoungLee <wnsrudals2346@gmail.com>
2024-01-09 17:52:18 +0900

    1-9
    작업

---
+++ src/main/webapp/resources/js/opA.js
@@ -0,0 +1,15 @@
+// 기준 온도 조절 버튼
+function temAd(){
+    var url = "temAdjust";
+    var name = "popup test";
+    var option = "width = 500, height = 500, top = 100, left = 200, location = no"
+    window.open(url, name, option);
+}
+// 기준 습도 조절 버튼
+function humAd(){
+	var url = "humAdjust";
+	var name = "popup test";
+	var option = "width = 500, height = 500, top = 100, left = 200, location = no"
+		window.open(url, name, option);
+}
+
